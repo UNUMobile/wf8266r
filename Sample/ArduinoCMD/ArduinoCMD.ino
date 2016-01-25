1f0cb1bc7bc158c9b9db4992119fc6aebf18ff74
UNU Product API <ckenson@gmail.com>
2016-01-25 23:12:30 +0800

    Unit11 : Serial/write service
    http://wf8266.com/wf8266r/tutorials/0B_Serial

---
+++ Sample/ArduinoCMD/ArduinoCMD.ino
@@ -0,0 +1,53 @@
+/*
+ WF8266R.js RESTful /serial/write service.
+*/
+
+#include <SoftwareSerial.h>
+SoftwareSerial wf8266r(2, 3); // RX, TX
+const uint8_t maxLength = 20;
+char inData[maxLength];
+char inChar=-1;
+byte index = 0;
+
+void setup() {
+  Serial.begin(9600);
+  wf8266r.begin(9600);
+
+  Serial.println("Ready");
+}
+
+void loop() {
+
+  if (Comp("GPIO13ON")) {
+    Serial.println("GPIO 13 ON");
+    digitalWrite(13,HIGH);
+  }
+  else if(Comp("GPIO13OFF")) {
+    Serial.println("GPIO 13 OFF");
+    digitalWrite(13,LOW);
+  }
+}
+
+boolean Comp(char* cmd) {
+  while (wf8266r.available() > 0)
+  {
+    if (index < maxLength -1)
+    {
+      inChar = wf8266r.read();
+      inData[index] = inChar;
+      index++;
+      inData[index] = '\0';
+    }
+  }
+
+  if (strcmp(inData, cmd)  == 0) {
+    for (int i = 0; i < 19; i++) {
+      inData[i] = 0;
+    }
+    index = 0;
+    return true;
+  }
+  else {
+    return false;
+  }
+}
