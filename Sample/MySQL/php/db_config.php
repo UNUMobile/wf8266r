854b37d100431952bf6773ab8f6aef6b3b73a722
UNU <ckenson@gmail.com>
2018-03-11 14:13:03 +0800

    default dbinfo

--- Sample/MySQL/php/db_config.php
+++ Sample/MySQL/php/db_config.php
@@ -1,7 +1,7 @@
 <?php
     global $_DB;	
-    $_DB['host'] = "sql100.byethost.com";
-    $_DB['username'] = "b12_21768697";
-    $_DB['password'] = "wf8266";
-    $_DB['dbname'] = "b12_21768697_demo";
+    $_DB['host'] = "your mysql ip";
+    $_DB['username'] = "user name";
+    $_DB['password'] = "password";
+    $_DB['dbname'] = "database name";
 ?>
